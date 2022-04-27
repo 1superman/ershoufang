d2658416661ac57e5041c1f3d9dee773dd233865
黄老爷 <614955896@qq.com>
2022-04-27 10:17:32 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -21,16 +21,17 @@ for c in quyu:
     for i in tqdm(range(1,101)):
         response = get_html(f'https://wh.lianjia.com/ershoufang/{c}/pg{i}/')
         selector = etree.HTML(response.content)
-#         jianjie = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[1]/a/text()')
-#         xiaoqu = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[2]/div/a[1]/text()')
-    #     quyu = [etree.HTML(get_html(i).content).xpath('/html/body/div[5]/div[2]/div[5]/div[2]/span[2]/a[1]/text()')[0] for i in url]
-#         data = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[3]/div/text()')
-#         huxing = [i.split(" | ")[0]  for i in data]
-#         mianji = [i.split(" | ")[1]  for i in data]
+        jianjie = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[1]/a/text()')
+        xiaoqu = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[2]/div/a[1]/text()')
+        quyu = [etree.HTML(get_html(i).content).xpath('/html/body/div[5]/div[2]/div[5]/div[2]/span[2]/a[1]/text()')[0] for i in url]
+        data = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[3]/div/text()')
+        huxing = [i.split(" | ")[0]  for i in data]
+        mianji = [i.split(" | ")[1]  for i in data]
         guanzhu = [i.split('/')[0] for i in selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[4]/text()')]
-#         fabusj = [i.split('/')[1].strip() for i in selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[4]/text()')]
+        fabusj = [i.split('/')[1].strip() for i in selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[4]/text()')]
         fangjia = [float(i) for i in selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[6]/div[1]/span/text()')]
         danjia = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[6]/div[2]/span/text()')
-#         chengqu = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[6]/div[2]/span/text()')
-        d = pd.DataFrame({"房价": fangjia, "单价/平":danjia, '关注人数': guanzhu})
+        chengqu = selector.xpath('//*[@id="content"]/div[1]/ul/li/div[1]/div[6]/div[2]/span/text()')
+        d = pd.DataFrame({"简介": jianjie, "小区": xiaoqu, "户型": huxing, "面积": mianji, "关注人数": guanzhu, 
+                          "发布时间": fabusj, "房价": fangjia, "单价/平":danjia, '区域':[c for i in jianjie]})
         df = pd.concat([df, d], ignore_index=True)
